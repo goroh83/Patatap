2e70b6dc8ebe8f219dc6b180e76b01fb264a215e
goroh83 <goroh83@yahoo.com>
2017-06-07 17:32:52 +0200

    circles function & array

--- myscript.js
+++ myscript.js
@@ -1,6 +1,17 @@
+var circles = [];
+
 function onKeyDown(event) {
     var maxPoint = new Point(view.size.width, view.size.height);
     var randomPoint = Point.random();
     var point = maxPoint * randomPoint;
-    new Path.Circle(point, 10).fillColor = 'red';
+    var newCircle = new Path.Circle(point, 500);
+    newCircle.fillColor = 'gold';
+    circles.push(newCircle);
+}
+
+function onFrame(event) {
+    for(var i  = 0; i<circles.length; i++) {
+    circles[i].fillColor.hue +=1;
+    circles[i].scale(.9);
+    }
 }
